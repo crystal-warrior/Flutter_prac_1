fa75f21e8e73e43b8f833c36aa03ab66e2c824c5
crystal-warrior <sparrowrow34@yandex.ru>
2025-10-03 18:19:23 +0300

    ElevatedButton Changed pt 2

--- lib/main.dart
+++ lib/main.dart
@@ -36,32 +36,18 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'ФИО - Хвостов Сергей Дмитриевич\nНомер группы - ИКБО-06-22\nНомер студенческого билета - 22И0468\n',
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.amber,
+        child: ElevatedButton(
+          onPressed: null,
+          child: const Text('Я кнопка, я кнопка, я кнопка!'),
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+            foregroundColor: MaterialStateProperty.all(Colors.white),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(25),
               ),
             ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: null,
-              child: const Text('Я кнопка, я кнопка, я кнопка!'),
-              style: ButtonStyle(
-                backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
-                foregroundColor: MaterialStateProperty.all(Colors.white),
-                shape: MaterialStateProperty.all(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25),
-                  ),
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
