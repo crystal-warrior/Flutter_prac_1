00081ec77a961b750a86f0d6214e19183aea7d1f
crystal-warrior <sparrowrow34@yandex.ru>
2025-10-03 23:09:24 +0300

    My own Widget Based on StatefullWidget Added

---
+++ lib/myownstatefull.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MyNotFirstButOneOfTheMostBeautifull extends StatefulWidget {
+
+
+  const MyNotFirstButOneOfTheMostBeautifull({super.key});
+
+  @override
+  State<MyNotFirstButOneOfTheMostBeautifull> createState() => _MyNotFirstButOneOfTheMostBeautifullState();
+}
+
+class _MyNotFirstButOneOfTheMostBeautifullState extends State<MyNotFirstButOneOfTheMostBeautifull> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
