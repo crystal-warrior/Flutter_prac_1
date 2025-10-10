9bf7b536a5a071a0b44742ec3eb37fd18e77f698
crystal-warrior <sparrowrow34@yandex.ru>
2025-10-10 19:44:49 +0300

    main.dart listView.builder widget added new buttons added old text deleted new text added

--- lib/main.dart
+++ lib/main.dart
@@ -21,52 +21,144 @@ class PlantApp extends StatelessWidget {
 }
 
 class GardenScreen extends StatelessWidget {
+  final List<Map<String, dynamic>> menuItems = [
+    {
+      'title': 'Каталог растений',
+      'icon': Icons.spa,
+      'screen': PlantCatalogScreen(),
+    },
+    {
+      'title': 'Полив',
+      'icon': Icons.water_drop,
+      'screen': WateringScreen(),
+    },
+    {
+      'title': 'Удобрения',
+      'icon': Icons.eco,
+      'screen': FertilizerScreen(),
+    },
+    {
+      'title': 'Советы по уходу',
+      'icon': Icons.help,
+      'screen': CareTipsScreen(),
+    },
+    {
+      'title': 'Мои растения',
+      'icon': Icons.agriculture,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Кнопка в разработке',
+      'icon': Icons.access_alarm_outlined,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Эксперимент',
+      'icon': Icons.account_balance,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 1',
+      'icon': Icons.add,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 2',
+      'icon': Icons.grass,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 3',
+      'icon': Icons.local_florist,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 4',
+      'icon': Icons.park,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 5',
+      'icon': Icons.nature,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 6',
+      'icon': Icons.forest,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 7',
+      'icon': Icons.emoji_nature,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 8',
+      'icon': Icons.yard,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 9',
+      'icon': Icons.eco,
+      'screen': MyPlantsScreen(),
+    },
+    {
+      'title': 'Дополнительная кнопка 10',
+      'icon': Icons.spoke_rounded,
+      'screen': MyPlantsScreen(),
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Мой Сад')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-
-            Text('Тестовый текст 1!',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold)),
-            Text('Тестовый текст 2!',
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-            Text('Тестовый текст 3!',
-                style: TextStyle(fontSize: 10, fontWeight: FontWeight.bold)),
-            SizedBox(height: 40),
-            _buildMenuButton(context, 'Каталог растений', Icons.spa, PlantCatalogScreen()),
-            _buildMenuButton(context, 'Полив', Icons.water_drop, WateringScreen()),
-            _buildMenuButton(context, 'Удобрения', Icons.eco, FertilizerScreen()),
-            _buildMenuButton(context, 'Советы по уходу', Icons.help, CareTipsScreen()),
-            _buildMenuButton(context, 'Мои растения', Icons.agriculture, MyPlantsScreen()),
-            _buildMenuButton(context, 'Кнопка в разработке', Icons.access_alarm_outlined, MyPlantsScreen()),
-            _buildMenuButton(context, 'Эксперимент', Icons.account_balance, MyPlantsScreen()),
-          ],
-        ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(16),
+        itemCount: menuItems.length + 1,
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return Align(
+              alignment: Alignment.center,
+              child: Text(
+                'Добро пожаловать в сад!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+            );
+          } else {
+            final menuIndex = index - 1;
+            final item = menuItems[menuIndex];
+            return _buildMenuButton(
+              context,
+              item['title'] as String,
+              item['icon'] as IconData,
+              item['screen'] as Widget,
+            );
+          }
+        },
       ),
     );
   }
 
   Widget _buildMenuButton(BuildContext context, String title, IconData icon, Widget screen) {
-    return Container(
-      width: 250,
-      margin: EdgeInsets.all(8),
-      child: ElevatedButton.icon(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
-        },
-        icon: Icon(icon),
-        label: Text(title),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: Colors.green,
-          foregroundColor: Colors.white,
-          padding: EdgeInsets.symmetric(vertical: 15),
+    return Center(
+      child: Container(
+        width: 250,
+        margin: EdgeInsets.all(8),
+        child: ElevatedButton.icon(
+          onPressed: () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) => screen));
+          },
+          icon: Icon(icon),
+          label: Text(title),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.green,
+            foregroundColor: Colors.white,
+            padding: EdgeInsets.symmetric(vertical: 15),
+          ),
         ),
       ),
     );
   }
-
 }
