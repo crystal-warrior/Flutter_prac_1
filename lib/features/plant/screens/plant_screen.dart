0c4f1cc04ad9204f560cc9a3b36e7f6530b58a81
crystal-warrior <sparrowrow34@yandex.ru>
2025-10-23 20:20:09 +0300

    CachedNetworkImage widget in plant_screen.dart

--- lib/features/plant/screens/plant_screen.dart
+++ lib/features/plant/screens/plant_screen.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import '../models/plant_model.dart';
 import '../widgets/plants_table.dart';
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class PlantsScreen extends StatelessWidget {
+
+
+  final String _imageUrl = "https://static.vecteezy.com/system/resources/previews/001/500/436/large_2x/many-plants-in-greenhouse-with-glass-wall-on-white-background-free-vector.jpg";
+
+
   final List<PlantModel> plants;
   final double averageComplexity;
   final VoidCallback onAdd;
@@ -24,6 +31,33 @@ class PlantsScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
+          Center(
+            child:
+            SizedBox(
+              width: 210,
+              height: 210,
+              child: CachedNetworkImage(
+                imageUrl: _imageUrl,
+                progressIndicatorBuilder: (context, url, progress) =>
+                const CircularProgressIndicator(),
+                errorWidget: (context, url, error) => const Center(
+                  child: Icon(
+                    Icons.error,
+                    color: Colors.red,
+                    size: 50,
+                  ),
+                ),
+                fit: BoxFit.contain,
+              ),
+            ),
+          ),
+
+
+
+
+
+
+
           Card(
             margin: const EdgeInsets.all(16),
             child: Padding(
