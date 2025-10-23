ee68f1f1966e947132fb361c0352d52b9bc9234e
crystal-warrior <sparrowrow34@yandex.ru>
2025-10-23 23:14:13 +0300

    Horizontal, Vertical navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,77 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:jhvostov_prac_1/app_router.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:jhvostov_prac_1/features/care_tips/screens/care_tips_screen.dart';
 import 'package:jhvostov_prac_1/features/fertilizers/screens/fertilizer_screen.dart';
 import 'package:jhvostov_prac_1/features/my_plants/screens/my_plants_screen.dart';
 import 'package:jhvostov_prac_1/features/plant/state/plants_container.dart';
 import 'package:jhvostov_prac_1/features/watering/screens/watering_screen.dart';
 import 'package:jhvostov_prac_1/shared/app_theme.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 void main() {
   runApp(PlantApp());
 }
 
 class PlantApp extends StatelessWidget {
-  PlantApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
+    return MaterialApp(
       title: 'Мой Сад',
       theme: AppTheme.lightTheme,
-      routerConfig: appRouter,
+      home: GardenScreen(),
     );
   }
 }
 
-class GardenScreen extends StatelessWidget {
-  final String _imageUrl = "https://ir.ozone.ru/s3/multimedia-b/6606452423.jpg";
+class GardenScreen extends StatefulWidget {
+  @override
+  _GardenScreenState createState() => _GardenScreenState();
+}
 
-  final List<Map<String, dynamic>> menuItems = [
-    {
-      'title': 'Мои растения',
-      'icon': Icons.favorite,
-      'route': 'my_plants',
-    },
-    {
-      'title': 'Полив',
-      'icon': Icons.water_drop,
-      'route': 'watering',
-    },
-    {
-      'title': 'Советы',
-      'icon': Icons.lightbulb,
-      'route': 'care_tips',
-    },
-    {
-      'title': 'Удобрения',
-      'icon': Icons.agriculture,
-      'route': 'fertilizers',
-    },
-    {
-      'title': 'Состояние растений',
-      'icon': Icons.local_florist_rounded,
-      'route': 'plant_status',
-    },
-  ];
+class _GardenScreenState extends State<GardenScreen> with SingleTickerProviderStateMixin {
+  final String _imageUrl = "https://ir.ozone.ru/s3/multimedia-b/6606452423.jpg";
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  void _navigateWithPop(Widget screen, String title) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (_) => Scaffold(
+          appBar: AppBar(
+            title: Text(title),
+            leading: IconButton(
+              icon: const Icon(Icons.arrow_back),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ),
+          body: screen,
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Мой Сад')),
-      body: ListView.builder(
-        padding: EdgeInsets.all(16),
-        itemCount: menuItems.length + 2,
-        itemBuilder: (context, index) {
-          if (index == 0) {
-            return Center(
-              child: Container(
-                margin: EdgeInsets.only(bottom: 20),
+      appBar: AppBar(
+        title: Text('Мой Сад'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Мои растения'),
+            Tab(text: 'Уход за растениями'),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        physics: const NeverScrollableScrollPhysics(),
+        children: [
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              Container(
+                margin: EdgeInsets.only(bottom: 30),
                 child: SizedBox(
-                  width: 200,
-                  height: 200,
+                  width: 150,
+                  height: 150,
                   child: CachedNetworkImage(
                     imageUrl: _imageUrl,
                     progressIndicatorBuilder: (context, url, progress) =>
@@ -87,49 +104,113 @@ class GardenScreen extends StatelessWidget {
                   ),
                 ),
               ),
-            );
-          } else if (index == 1) {
-            return Align(
-              alignment: Alignment.center,
-              child: Container(
+
+              Container(
                 margin: EdgeInsets.only(bottom: 30),
                 child: Text(
                   'Добро пожаловать в Сад!',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                   textAlign: TextAlign.center,
                 ),
               ),
-            );
-          } else {
-            final menuIndex = index - 2;
-            final item = menuItems[menuIndex];
-            return _buildMenuButton(
-              context,
-              item['title'] as String,
-              item['icon'] as IconData,
-              item['route'] as String,
-            );
-          }
-        },
-      ),
-    );
-  }
 
-  Widget _buildMenuButton(BuildContext context, String title, IconData icon, String routeName) {
-    return Center(
-      child: Container(
-        width: 250,
-        margin: EdgeInsets.all(8),
-        child: ElevatedButton.icon(
-          onPressed: () {
-            context.pushNamed(routeName);
-          },
-          icon: Icon(icon),
-          label: Text(title),
-          style: ElevatedButton.styleFrom(
-            padding: EdgeInsets.symmetric(vertical: 15),
+              Container(
+                width: 250,
+                child: ElevatedButton.icon(
+                  onPressed: () => _navigateWithPop(PlantsContainer(), 'Мои растения'),
+                  icon: Icon(Icons.favorite),
+                  label: Text('Все растения'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 250,
+                child: ElevatedButton.icon(
+                  onPressed: () => _navigateWithPop(MyPlantsScreen(), 'Состояние растений'),
+                  icon: Icon(Icons.local_florist_rounded),
+                  label: Text('Состояние растений'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ),
+
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(bottom: 30),
+                child: SizedBox(
+                  width: 150,
+                  height: 150,
+                  child: CachedNetworkImage(
+                    imageUrl: _imageUrl,
+                    progressIndicatorBuilder: (context, url, progress) =>
+                    const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) => const Center(
+                      child: Icon(
+                        Icons.error,
+                        color: Colors.red,
+                        size: 50,
+                      ),
+                    ),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+
+              // Приветствие
+              Container(
+                margin: EdgeInsets.only(bottom: 30),
+                child: Text(
+                  'Уход за растениями',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+
+              Container( // вертикальная навигация
+                child: ElevatedButton.icon(
+                  onPressed: () => _navigateWithPop(WateringScreen(), 'Полив'),
+                  icon: Icon(Icons.water_drop),
+                  label: Text('Полив растений'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 250,
+                child: ElevatedButton.icon(
+                  onPressed: () => _navigateWithPop(FertilizerScreen(), 'Удобрения'),
+                  icon: Icon(Icons.agriculture),
+                  label: Text('Удобрения'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                ),
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: 250,
+                child: ElevatedButton.icon(
+                  onPressed: () => _navigateWithPop(CareTipsScreen(), 'Советы по уходу'),
+                  icon: Icon(Icons.lightbulb),
+                  label: Text('Советы по уходу'),
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
