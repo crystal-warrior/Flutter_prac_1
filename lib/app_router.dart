0a2fc686cb34d590f149847da8f3f92b20203af8
crystal-warrior <sparrowrow34@yandex.ru>
2025-10-31 19:42:29 +0300

    Horizontal marshrut navigation added

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,96 +1,128 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
-import 'features/care_tips/screens/care_tips_screen.dart';
-import 'features/fertilizers/screens/fertilizer_screen.dart';
-import 'features/my_plants/screens/my_plants_screen.dart';
-import 'features/plant/state/plants_container.dart';
-import 'features/watering/screens/watering_screen.dart';
 import 'main.dart';
 
 final GoRouter appRouter = GoRouter(
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/',
-      name: 'garden',
-      builder: (context, state) => GardenScreen(),
+    ShellRoute(
+      builder: (context, state, child) {
+        int tabIndex = 0;
+        final location = state.uri.toString();
+
+        if (location.contains('/my_plants')) tabIndex = 0;
+        else if (location.contains('/watering')) tabIndex = 1;
+        else if (location.contains('/fertilizers')) tabIndex = 2;
+        else if (location.contains('/care_tips')) tabIndex = 3;
+        else if (location.contains('/plant_status')) tabIndex = 4;
+
+        return GardenScreen(initialTab: tabIndex);
+      },
       routes: [
         GoRoute(
-          path: 'my_plants',
+          path: '/',
+          name: 'home',
+          builder: (context, state) => Container(),
+        ),
+        GoRoute(
+          path: '/my_plants',
           name: 'my_plants',
-          builder: (context, state) => Scaffold(
-            appBar: AppBar(
-              title: Text('Мои растения'),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => context.pop(),
-              ),
-            ),
-            body: PlantsContainer(),
-          ),
+          builder: (context, state) => Container(),
         ),
-
         GoRoute(
-          path: 'watering',
+          path: '/watering',
           name: 'watering',
-          builder: (context, state) => Scaffold(
-            appBar: AppBar(
-              title: Text('Полив'),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => context.pop(),
-              ),
-            ),
-            body: WateringScreen(),
-          ),
+          builder: (context, state) => Container(),
         ),
-
         GoRoute(
-          path: 'care_tips',
-          name: 'care_tips',
-          builder: (context, state) => Scaffold(
-            appBar: AppBar(
-              title: Text('Советы по уходу'),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => context.pop(),
-              ),
-            ),
-            body: CareTipsScreen(),
-          ),
-        ),
-
-        GoRoute(
-          path: 'fertilizers',
+          path: '/fertilizers',
           name: 'fertilizers',
-          builder: (context, state) => Scaffold(
-            appBar: AppBar(
-              title: Text('Удобрения'),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => context.pop(),
-              ),
-            ),
-            body: FertilizerScreen(),
-          ),
+          builder: (context, state) => Container(),
         ),
-
         GoRoute(
-          path: 'plant_status',
+          path: '/care_tips',
+          name: 'care_tips',
+          builder: (context, state) => Container(),
+        ),
+        GoRoute(
+          path: '/plant_status',
           name: 'plant_status',
-          builder: (context, state) => Scaffold(
-            appBar: AppBar(
-              title: Text('Состояние растений'),
-              leading: IconButton(
-                icon: const Icon(Icons.arrow_back),
-                onPressed: () => context.pop(),
-              ),
-            ),
-            body: MyPlantsScreen(),
-          ),
+          builder: (context, state) => Container(),
         ),
       ],
     ),
+
+    GoRoute(
+      path: '/plant_detail',
+      name: 'plant_detail',
+      builder: (context, state) {
+        final plantName = state.uri.queryParameters['name'] ?? 'Растение';
+        final plantDescription = state.uri.queryParameters['description'] ?? 'Описание';
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text('Детали растения'),
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              onPressed: () => context.pop(),
+            ),
+          ),
+          body: Padding(
+            padding: EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  plantName,
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16),
+                Text(
+                  plantDescription,
+                  style: TextStyle(fontSize: 16),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () => context.pop(),
+                  child: Text('Вернуться назад'),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    ),
   ],
 );
+
+class AppNavigation {
+  static void goToMyPlants(BuildContext context) {
+    context.go('/my_plants');
+  }
+
+  static void goToWatering(BuildContext context) {
+    context.go('/watering');
+  }
+
+  static void goToFertilizers(BuildContext context) {
+    context.go('/fertilizers');
+  }
+
+  static void goToCareTips(BuildContext context) {
+    context.go('/care_tips');
+  }
+
+  static void goToPlantStatus(BuildContext context) {
+    context.go('/plant_status');
+  }
+
+  static void pushToPlantDetail(BuildContext context, {required String name, required String description}) {
+    context.push(
+      '/plant_detail?name=${Uri.encodeComponent(name)}&description=${Uri.encodeComponent(description)}',
+    );
+  }
+
+  static void goBack(BuildContext context) {
+    context.pop();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:jhvostov_prac_1/app_router.dart';
 import 'package:jhvostov_prac_1/shared/app_theme.dart';
-
 import 'features/care_tips/screens/care_tips_screen.dart';
 import 'features/fertilizers/screens/fertilizer_screen.dart';
 import 'features/my_plants/screens/my_plants_screen.dart';
@@ -16,81 +17,104 @@ class PlantApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'Мой Сад',
       theme: AppTheme.lightTheme,
-      home: MainTabScreen(initialTab: 0),
+      routerConfig: appRouter,
     );
   }
 }
 
-class MainTabScreen extends StatefulWidget {
-  final int initialTab;
+class GardenScreen extends StatefulWidget {
+  final int? initialTab;
 
-  const MainTabScreen({super.key, required this.initialTab});
+  const GardenScreen({super.key, this.initialTab});
 
   @override
-  _MainTabScreenState createState() => _MainTabScreenState();
+  _GardenScreenState createState() => _GardenScreenState();
 }
 
-class _MainTabScreenState extends State<MainTabScreen> with SingleTickerProviderStateMixin {
+class _GardenScreenState extends State<GardenScreen> with SingleTickerProviderStateMixin {
+  final String _imageUrl = "https://ir.ozone.ru/s3/multimedia-b/6606452423.jpg";
   late TabController _tabController;
+  int _currentTabIndex = 0;
 
   final List<Map<String, dynamic>> _tabs = [
     {
       'title': 'Мои растения',
       'icon': Icons.favorite,
       'widget': PlantsContainer(),
+      'route': '/my_plants',
     },
     {
       'title': 'Полив',
       'icon': Icons.water_drop,
       'widget': WateringScreen(),
+      'route': '/watering',
     },
     {
       'title': 'Удобрения',
       'icon': Icons.agriculture,
       'widget': FertilizerScreen(),
+      'route': '/fertilizers',
     },
     {
       'title': 'Советы по уходу',
       'icon': Icons.lightbulb,
       'widget': CareTipsScreen(),
+      'route': '/care_tips',
     },
     {
       'title': 'Состояние',
       'icon': Icons.eco,
       'widget': MyPlantsScreen(),
+      'route': '/plant_status',
     },
   ];
 
   @override
   void initState() {
     super.initState();
-    _tabController = TabController(length: _tabs.length, vsync: this, initialIndex: widget.initialTab);
+
+
+    final initialIndex = widget.initialTab ?? 0;
+    _currentTabIndex = initialIndex;
+    _tabController = TabController(length: _tabs.length, vsync: this, initialIndex: initialIndex);
+
+    _tabController.addListener(_handleTabSelection);
   }
 
+  void _handleTabSelection() {
+    if (_tabController.indexIsChanging) {
+      setState(() {
+        _currentTabIndex = _tabController.index;
+      });
 
-  void _navigateToTab(int index) {
-    if (index == _tabController.index) return;
+      final routeName = _tabs[_tabController.index]['route'];
+      context.go(routeName);
+    }
+  }
 
-    Navigator.pushReplacement(
-      context,
-      MaterialPageRoute(
-        builder: (context) => MainTabScreen(initialTab: index),
-      ),
-    );
+  @override
+  void dispose() {
+    _tabController.removeListener(_handleTabSelection);
+    _tabController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text(_tabs[_currentTabIndex]['title']),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         bottom: TabBar(
           controller: _tabController,
           isScrollable: true,
-          onTap: _navigateToTab,
+          onTap: (index) {
+            final routeName = _tabs[index]['route'];
+            context.go(routeName);
+          },
           tabs: _tabs.map((tab) => Tab(
             icon: Icon(tab['icon']),
             text: tab['title'],
@@ -102,11 +126,5 @@ class _MainTabScreenState extends State<MainTabScreen> with SingleTickerProvider
         children: _tabs.map<Widget>((tab) => tab['widget']).toList(),
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
   }
 }
