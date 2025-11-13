5e70a22bc1f0229f25a48adab1e7c3b637bd04ae
crystal-warrior <sparrowrow34@yandex.ru>
2025-11-13 23:08:37 +0300

    bug fixes

--- lib/features/authorization/autorization.dart
+++ lib/features/authorization/autorization.dart
@@ -3,43 +3,54 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:jhvostov_prac_1/auth/cubit/auth_cubit.dart';
-import '../../di/service_locator.dart';
 
 class AuthorizationScreen extends StatelessWidget {
   const AuthorizationScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final TextEditingController _controller = TextEditingController();
+    final TextEditingController _loginController = TextEditingController();
+    final TextEditingController _passwordController = TextEditingController();
 
     return Scaffold(
-      appBar: AppBar(title: const Text('Авторизация'), backgroundColor: Colors.lightGreen),
+      appBar: AppBar(
+        title: const Text('Авторизация'),
+        backgroundColor: Colors.lightGreen,
+      ),
       body: Padding(
         padding: const EdgeInsets.all(24.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('Введите ваш логин', style: TextStyle(fontSize: 18)),
+            const Text('Введите ваши данные', style: TextStyle(fontSize: 18)),
             const SizedBox(height: 20),
+
+            // Поле логина
             TextField(
-              controller: _controller,
+              controller: _loginController,
               decoration: const InputDecoration(
                 labelText: 'Логин',
                 border: OutlineInputBorder(),
               ),
             ),
+            const SizedBox(height: 16),
+
+
+            TextField(
+              controller: _passwordController,
+              obscureText: true,
+              decoration: const InputDecoration(
+                labelText: 'Пароль',
+                border: OutlineInputBorder(),
+              ),
+            ),
             const SizedBox(height: 30),
+
             ElevatedButton(
               onPressed: () {
-                final login = _controller.text.trim();
+                final login = _loginController.text.trim();
                 if (login.isNotEmpty) {
-
-                 // locator<UserService>().setLogin(login);
-
-
                   context.read<AuthCubit>().setLogin(login);
-
-
                   context.go('/garden');
                 }
               },
